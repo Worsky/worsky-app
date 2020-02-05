4da106e9dff327f0d0095ae8bb57c11de6cc7a21
Bruno Cardoso <bruno@liveonsolutions.com>
2020-02-05 21:17:20 -0200

    pendencias android

--- android/app/src/main/java/com/pilotobrasil/MainApplication.java
+++ android/app/src/main/java/com/pilotobrasil/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import com.reactlibrary.RNCompassHeadingPackage;
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.webview.RNCWebViewPackage;
+import com.agontuk.RNFusedLocation.RNFusedLocationPackage;
 import com.sensors.RNSensorsPackage;
 import com.mapbox.rctmgl.RCTMGLPackage;
 import com.reactnativecommunity.geolocation.GeolocationPackage;
@@ -21,48 +23,53 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.mapbox.rctmgl.RCTMGLPackage;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
-    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-        @Override
-        public boolean getUseDeveloperSupport() {
-            return BuildConfig.DEBUG;
-        }
-
-        @Override
-        protected List<ReactPackage> getPackages() {
-            return Arrays.<ReactPackage>asList(new MainReactPackage(),
-            new RNSensorsPackage(),
-            new RCTMGLPackage(),
-            new GeolocationPackage(),
-            new ReactNativeOneSignalPackage(),
-            new RNFirebasePackage(),
-            new ReactVideoPackage(),
-            new ImagePickerPackage(),
-            new ReanimatedPackage(),
-            new TestFairyPackage(),
-            new VectorIconsPackage(), new RNGestureHandlerPackage(), RNCompassHeadingPackage());
-        }
-
-        @Override
-        protected String getJSMainModuleName() {
-            return "index";
-        }
-    };
-
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
-    public ReactNativeHost getReactNativeHost() {
-        return mReactNativeHost;
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
     }
 
     @Override
-    public void onCreate() {
-        super.onCreate();
-        SoLoader.init(this, /* native exopackage */ false);
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+        new MainReactPackage(),
+        new RNCWebViewPackage(),
+        new RNFusedLocationPackage(),
+        new RNSensorsPackage(),
+        new RCTMGLPackage(),
+        new GeolocationPackage(),
+        new ReactNativeOneSignalPackage(),
+        new RNFirebasePackage(),
+        new ReactVideoPackage(),
+        new ImagePickerPackage(),
+        new ReanimatedPackage(),
+        new TestFairyPackage(),
+        new VectorIconsPackage(),
+        new RNGestureHandlerPackage(),
+        new RNCompassHeadingPackage()
+      );
     }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
 }
