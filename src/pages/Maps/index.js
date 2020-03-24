2b22357656528f71d87c0e56d3b5db2e0195c550
Bruno Cardoso <bruno@liveonsolutions.com>
2020-03-24 07:30:40 -0300

    add components to map

--- src/pages/Maps/index.js
+++ src/pages/Maps/index.js
@@ -1,9 +1,18 @@
 import React, { useEffect, useState, useRef } from "react";
-import { StatusBar, Platform, View, Text, Alert } from "react-native";
+import { StatusBar, Platform, View, Alert } from "react-native";
 import MapboxGL from "@react-native-mapbox-gl/maps";
 import Geolocation from "@react-native-community/geolocation";
+import CompassHeading from "react-native-compass-heading";
 
-import { colors } from "~/styles";
+import MapTool from "~components/MapTool";
+
+import { plane } from "~/assets";
+
+import {
+  dispatchAndVerifyPermissions,
+  errorMarginToDisplayTargetIcon
+} from "./helpers";
+import styles from "./styles";
 
 MapboxGL.setAccessToken(
   "pk.eyJ1Ijoid29yc2t5IiwiYSI6ImNrN3dwb2xvMjA0ZDQza3FncDhnY3BocnkifQ.3s1eTwHlWIbhWjDiTfp2wQ"
@@ -11,41 +20,96 @@ MapboxGL.setAccessToken(
 
 export default function Maps() {
   const [currentPosition, setCurrentPosition] = useState({});
+  const [speed, setSpeed] = useState(0);
+  const [altitude, setAltitude] = useState(0);
+  const [heading, setHeading] = useState(0);
+  const [follow, setFollow] = useState(true);
   const refCamera = useRef(null);
-  // let watchID = null;
+  const refMapView = useRef(null);
 
-  // useEffect(() => {
-  //   watchID = Geolocation.watchPosition(
-  //     ({ coords }) => {
-  //       setCurrentPosition({
-  //         latitude: coords.latitude,
-  //         longitute: coords.longitude
-  //       });
-  //     },
-  //     error => Alert.alert(JSON.stringify(error)),
-  //     {
-  //       timeout: 3000,
-  //       maximumAge: 0,
-  //       distanceFilter: 2,
-  //       useSignificantChanges: true
-  //     }
-  //   );
-  // }, []);
+  const centerMapOnMe = async () => {
+    try {
+      if (follow) setFollow(false);
 
-  // useEffect(() => {
-  //   if (currentPosition.hasOwnProperty("latitude"))
-  //     refCamera.current.flyTo([
-  //       currentPosition.longitude,
-  //       currentPosition.latitude
-  //     ]);
-  // }, [currentPosition]);
+      await mapCamera.moveTo(
+        [userPosition.longitude, userPosition.latitude],
+        1200
+      );
+
+      setFollow(true);
+    } catch (error) {
+      setFollow(true);
+    }
+  };
+
+  const handleCenterPosition = ({ coords }) => {
+    setAltitude(coords.altitude);
+
+    setSpeed(coords.speed);
+
+    setCurrentPosition({
+      latitude: coords.latitude,
+      longitute: coords.longitude
+    });
+
+    refCamera.current.flyTo([coords.longitude, coords.latitude]);
+  };
+
+  const onRegionDidChanges = async () => {
+    const [_longitude, _latitude] = await refMapView.getCenter();
+
+    Geolocation.getCurrentPosition(async ({ coords }) => {
+      const { longitude, latitude } = coords;
+
+      const mapViewCenter = { _longitude, _latitude };
+      const userCurrentPosition = { longitude, latitude };
+
+      const shouldFollowUpdate = errorMarginToDisplayTargetIcon(
+        mapViewCenter,
+        userCurrentPosition
+      );
+
+      if (!shouldFollowUpdate) await setFollow(shouldFollowUpdate);
+    });
+
+    if (!follow) handleMapPan();
+  };
+
+  useEffect(() => {
+    // Geolocation.watchPosition(
+    //   ({ coords }) => {
+    //     setCurrentPosition({
+    //       latitude: coords.latitude,
+    //       longitute: coords.longitude
+    //     });
+    //   },
+    //   error => Alert.alert(JSON.stringify(error)),
+    //   {
+    //     timeout: 3000,
+    //     maximumAge: 0,
+    //     distanceFilter: 2,
+    //     useSignificantChanges: true
+    //   }
+    // );
+    dispatchAndVerifyPermissions();
+
+    CompassHeading.start(3, degree => {
+      setHeading(degree);
+    });
+  }, []);
 
   return (
     <View style={{ flex: 1 }}>
-      <MapboxGL.MapView style={{ flex: 1 }} styleURL={MapboxGL.StyleURL.Dark}>
+      <MapboxGL.MapView
+        style={{ flex: 1 }}
+        styleURL={MapboxGL.StyleURL.Dark}
+        ref={refMapView}
+        // onPress={cleanSearchAndCenterMap}
+        onRegionDidChange={onRegionDidChanges}
+      >
         <MapboxGL.Camera
           followUserLocation
-          followUserMode={MapboxGL.UserTrackingModes.FollowWithCourse}
+          followUserMode={MapboxGL.UserTrackingModes.FollowWithHeading}
           centerCoordinate={[
             currentPosition.longitute,
             currentPosition.latitude
@@ -53,17 +117,27 @@ export default function Maps() {
           zoomLevel={15}
           ref={refCamera}
         />
-        <MapboxGL.UserLocation
-          onUpdate={({ coords }) => {
-            setCurrentPosition({
-              latitude: coords.latitude,
-              longitute: coords.longitude
-            });
-            refCamera.current.flyTo([coords.longitude, coords.latitude]);
-          }}
-        />
+        <MapboxGL.UserLocation onUpdate={handleCenterPosition} />
       </MapboxGL.MapView>
-      <Text>{JSON.stringify({ ...currentPosition })}</Text>
+
+      <Image source={plane} style={styles.planeOnMap} height={64} width={64} />
+
+      {!follow && (
+        <TouchableOpacity
+          style={styles.myPositionButton}
+          onPress={centerMapOnMe}
+        >
+          <Icon name="crosshairs" size={22} color={"black"} />
+        </TouchableOpacity>
+      )}
+
+      <View style={styles.instruments}>
+        <MapTool text={`${speed.toFixed(1)} kt`} />
+
+        <MapTool text={`${Math.round(heading || 0)}º`} />
+
+        <MapTool text={`${Math.round(altitude * 3.2808)} ft`} />
+      </View>
     </View>
   );
 }
---
+++ src/pages/Maps/styles.js
@@ -0,0 +1,150 @@
+import { StyleSheet, Platform } from "react-native";
+import { colors, metrics } from "~/styles";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+
+  mapContainer: {
+    height: metrics.height,
+    width: metrics.width
+  },
+
+  searchContainer: {
+    flex: 1,
+    flexDirection: "row",
+    position: "absolute",
+    // zIndex: 99,
+    padding: metrics.basePadding / 2,
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: metrics.width,
+    backgroundColor: colors.worSky.white,
+    ...Platform.select({
+      ios: {
+        paddingTop: 35
+      },
+      android: {
+        // paddingRight: 20
+      }
+    })
+  },
+
+  iconContainer: {
+    height: 28,
+    width: 30,
+    marginLeft: 5,
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+    ...Platform.select({
+      ios: {
+        position: "absolute",
+        top: 47,
+        right: 30,
+        zIndex: 999
+      },
+      android: {
+        paddingTop: 35
+      }
+    })
+  },
+
+  filterIconContainer: {
+    width: 60,
+    height: 40,
+    marginTop: 10,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
+  autocomplete: {
+    height: Platform.OS == "ios" ? 30 : 35,
+    width: metrics.width - 80,
+    marginTop: metrics.baseMargin,
+    backgroundColor: colors.worSky.lighter,
+    borderRadius: metrics.baseRadius * 6,
+    textAlign: "center",
+    paddingHorizontal: 20,
+    flex: 1,
+    alignContent: "center"
+  },
+
+  instruments: {
+    position: "absolute",
+    left: 4,
+    right: 4,
+    top: 70,
+    backgroundColor: "transparent",
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+
+  instrumentItem: {
+    color: "#333",
+    fontWeight: "bold",
+    fontSize: 22,
+    height: 40,
+    backgroundColor: "white",
+    borderRadius: 20,
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "rgba(0,0,0,0.5)",
+    flex: 1,
+    alignContent: "center",
+    justifyContent: "center",
+    marginTop: 8,
+    marginHorizontal: 4,
+    textAlign: "center",
+    lineHeight: 40,
+    overflow: "hidden"
+  },
+
+  myPositionButton: {
+    flex: 1,
+    position: "absolute",
+    right: 8,
+    bottom: 5,
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: "white",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 5
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
+  myHeadButton: {
+    position: "absolute",
+    left: 8,
+    bottom: 8,
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: "white",
+    borderColor: "rgba(0,0,0,0.5)",
+    borderWidth: 1,
+    backgroundColor: "white",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
+  planeOnMap: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: [{ translateX: -32 }, { translateY: -32 }]
+  }
+});
+
+export default styles;
